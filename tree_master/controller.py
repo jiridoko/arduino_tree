70935873194bee112b1832526ced6da5107bd91a
Jiri Dokoupil <33521168+jiridoko@users.noreply.github.com>
2017-11-12 12:37:44 +0000

    adding persistent storage

--- tree_master/controller.py
+++ tree_master/controller.py
@@ -9,17 +9,27 @@ from threading import Thread
 from time import sleep
 from ledmaster import led_master
 from logging.handlers import RotatingFileHandler
+from value_store import storage
 
 app = Flask(__name__, static_url_path='')
 
-global ramp_up
-global calm_down
-global retention
-ramp_up = 20
-calm_down = 2
-retention = 10
+    #s = storage('test2.out')
+    #print str(s.get_value("test"))
+    #s.set_value("test", "val")
+    #print str(s.get_value("test"))
+    #s.set_value("test2", 56)
+    #print str(int(s.get_value("test2"))+2)
 
-led = led_master(ramp_up_speed=ramp_up, calm_down_speed=calm_down, retention=retention)
+global s
+s = storage('config.ini')
+if s.get_value("ramp_up") is None:
+    s.set_value("ramp_up", 20)
+if s.get_value("calm_down") is None:
+    s.set_value("calm_down", 2)
+if s.get_value("retention") is None:
+    s.set_value("retention", 10)
+
+led = led_master(ramp_up_speed=int(s.get_value("ramp_up")), calm_down_speed=int(s.get_value("calm_down")), retention=int(s.get_value("retention")))
 
 class animator(Thread):
     def __init__(self, led):
@@ -52,36 +62,33 @@ global function_list
 function_list = [ ('button', 'Off', 'Select', True, 1, None),
                   ('button', 'Snake', 'Select', False, 2, None),
                   ('button', 'Snowfall', 'Select', False, 3, None),
-                  ('plusminus', 'Ramp up: 20', None, None, 4, 5),
-                  ('plusminus', 'Calm down: 2', None, None, 6, 7),
-                  ('plusminus', 'Retention: 10', None, None, 8, 9),
+                  ('plusminus', 'Ramp up: '+s.get_value("ramp_up"), None, None, 4, 5),
+                  ('plusminus', 'Calm down: '+s.get_value("calm_down"), None, None, 6, 7),
+                  ('plusminus', 'Retention: '+s.get_value("retention"), None, None, 8, 9),
                   ]
 
 def update_ramp_up(new_value):
-    global ramp_up
-    ramp_up = new_value
+    s.set_value("ramp_up", new_value)
     led.set_ramp_up_speed(new_value)
     global function_list
     elementType, nameL, nameR, highlighted, idPlus, idMinus = function_list[3]
-    nameL = "Ramp up: "+str(ramp_up)
+    nameL = "Ramp up: "+str(new_value)
     function_list[3] = (elementType, nameL, nameR, highlighted, idPlus, idMinus)
 
 def update_calm_down(new_value):
-    global calm_down
-    calm_down = new_value
+    s.set_value("calm_down", new_value)
     led.set_calm_down_speed(new_value)
     global function_list
     elementType, nameL, nameR, highlighted, idPlus, idMinus = function_list[4]
-    nameL = "Calm down: "+str(calm_down)
+    nameL = "Calm down: "+str(new_value)
     function_list[4] = (elementType, nameL, nameR, highlighted, idPlus, idMinus)
 
 def update_retention(new_value):
-    global retention
-    retention = new_value
+    s.set_value("retention", new_value)
     led.set_retention(new_value)
     global function_list
     elementType, nameL, nameR, highlighted, idPlus, idMinus = function_list[5]
-    nameL = "Retention: "+str(retention)
+    nameL = "Retention: "+str(new_value)
     function_list[5] = (elementType, nameL, nameR, highlighted, idPlus, idMinus)
 
 def update_highlight(index, set_to):
@@ -107,28 +114,31 @@ def button(path):
         update_highlight(1, False)
         update_highlight(2, False)
         ani.set_showing(False)
+        s.set_value("mode", "off")
     elif path == "2":
         update_highlight(0, False)
         update_highlight(1, True)
         update_highlight(2, False)
         ani.set_showing(True)
+        s.set_value("mode", "snake")
     elif path == "3":
         update_highlight(0, False)
         update_highlight(1, False)
         update_highlight(2, True)
         ani.set_showing(False)
+        s.set_value("mode", "snow")
     elif path == "4":
-        update_ramp_up(ramp_up+1)
+        update_ramp_up(int(s.get_value("ramp_up"))+1)
     elif path == "5":
-        update_ramp_up(ramp_up-1)
+        update_ramp_up(int(s.get_value("ramp_up"))-1)
     elif path == "6":
-        update_calm_down(calm_down+1)
+        update_calm_down(int(s.get_value("calm_down"))+1)
     elif path == "7":
-        update_calm_down(calm_down-1)
+        update_calm_down(int(s.get_value("calm_down"))-1)
     elif path == "8":
-        update_retention(retention+1)
+        update_retention(int(s.get_value("retention"))+1)
     elif path == "9":
-        update_retention(retention-1)
+        update_retention(int(s.get_value("retention"))-1)
     else:
         update_highlight(0, False);
         update_highlight(1, False);
@@ -141,6 +151,23 @@ if __name__ == '__main__':
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     try:
+        program = s.get_value("mode", default=None)
+        if program == "off":
+            update_highlight(0, True)
+            update_highlight(1, False)
+            update_highlight(2, False)
+            ani.set_showing(False)
+        elif program == "snake":
+            update_highlight(0, False)
+            update_highlight(1, True)
+            update_highlight(2, False)
+            ani.set_showing(True)
+        elif program == "snow":
+            update_highlight(0, False)
+            update_highlight(1, False)
+            update_highlight(2, True)
+            ani.set_showing(False)
+
         app.run(host="0.0.0.0", port=80)
     except KeyboardInterrupt:
         print "interrupt"
---
+++ tree_master/value_store.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2
+
+import ConfigParser
+import os
+
+class storage(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.c = ConfigParser.ConfigParser()
+        self._load()
+
+    def _load(self):
+        if os.path.isfile(self.filename):
+            self.c.read(self.filename)
+
+    def _store(self):
+        with open(self.filename, 'w') as f:
+            self.c.write(f)
+            
+    def _ConfigSectionMap(self, section):
+        dict1 = {}
+        try:
+            options = self.c.options(section)
+            for option in options:
+                try:
+                    dict1[option] = self.c.get(section, option)
+                    if dict1[option] == -1:
+                        #print "skip: %s" % option
+                        pass
+                except:
+                    dict1[option] = None
+        except ConfigParser.NoSectionError:
+            dict1[section] = None
+        return dict1
+
+    def set_value(self, name, value):
+        try:
+            options = self.c.options("main")
+        except:
+            self.c.add_section("main")
+        self.c.set("main", name, str(value))
+        self._store()
+
+    def get_value(self, name, default=None):
+        retval = default
+        try:
+            retval = self._ConfigSectionMap("main")[name]
+        except KeyError:
+            return default
+        return retval
+
+if __name__ == "__main__":
+    s = storage('test2.out')
+    print str(s.get_value("test"))
+    s.set_value("test", "val")
+    print str(s.get_value("test"))
+    s.set_value("test2", 56)
+    print str(int(s.get_value("test2"))+2)
