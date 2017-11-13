a7d84ecc17825ef1dae1f4cb5122e88416d97fdd
Jiří Dokoupil <jiridoko@users.noreply.github.com>
2017-11-13 11:26:11 +0000

    should work now

--- tree_master/animations/__animation.py
+++ tree_master/animations/__animation.py
@@ -19,20 +19,20 @@ class animation(Thread):
         self.RETENTION_MAX = 1000
         
         if not blank_args:
-            self.initialize_argument("ramp_up", default_value=20)
+            self.initialize_argument("ramp_up", "Ramp up: ", default_value=20)
             self.led.set_ramp_up_speed(self.get_argument("ramp_up"))
-            self.initialize_argument("calm_down", default_value=2)
+            self.initialize_argument("calm_down", "Calm down: ", default_value=2)
             self.led.set_calm_down_speed(self.get_argument("calm_down"))
-            self.initialize_argument("retention", default_value=10)
+            self.initialize_argument("retention", "Retention: ", default_value=10)
             self.led.set_retention(self.get_argument("retention"))
     def disable(self):
         self.enabled = False
-    def initialize_argument(self, arg_name, default_value=None):
-        self.arguments.append(arg_name)
+    def initialize_argument(self, arg_name, human_readable, default_value=None):
+        self.arguments.append((arg_name, human_readable))
         if self.storage.get_value(arg_name, section=self.animation_name) is None:
             self.storage.set_value(arg_name, default_value, section=self.animation_name)
     def set_argument(self, arg_name, arg_value):
-        if arg_name in self.arguments:
+        if arg_name in self.get_argument_ids():
             if arg_name == "ramp_up" and arg_value >= self.RAMP_UP_MIN and arg_value <= self.RAMP_UP_MAX:
                 self.led.set_ramp_up_speed(arg_value)
             elif arg_name == "ramp_up":
@@ -49,7 +49,9 @@ class animation(Thread):
     def get_argument_list(self):
         return self.arguments
     def get_argument(self, arg_name):
-        if arg_name in self.arguments:
+        if arg_name in self.get_argument_ids():
             return self.storage.get_value(arg_name, section=self.animation_name)
         else:
             return None
+    def get_argument_ids(self):
+        return [identifier for identifier, name in self.arguments]
