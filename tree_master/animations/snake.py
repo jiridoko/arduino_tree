bc5c13c7a9babf51b084d47b840e7a2125f1a411
Jiří Dokoupil <jiridoko@users.noreply.github.com>
2017-11-13 16:31:21 +0000

    Adding speed to snake

--- tree_master/animations/snake.py
+++ tree_master/animations/snake.py
@@ -6,10 +6,12 @@ import __animation
 class snake(__animation.animation):
     def __init__(self, led, storage):
         super(snake, self).__init__(led, storage, "snake")
+        self.initialize_argument("speed", "Speed: ", default_value=20)
     def run(self):
         while self.enabled:
             for i in xrange(0,80):
                 self.led.set_led_value(i, self.led.CONST_MAX_INTENSITY)
-                sleep(0.05)
+                sleep_length = 1.00 / int(self.get_argument("speed"))
+                sleep(sleep_length)
                 if not self.enabled:
                     break
