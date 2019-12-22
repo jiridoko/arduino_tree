e1ff79254219ca8d8fed34a9f9f6f06bc4b18bce
Jiri Dokoupil <33521168+jiridoko@users.noreply.github.com>
2019-12-22 22:22:48 +0000

    fixing random colours ... adding more red for whatever reason

--- tree_master/animations/rand.py
+++ tree_master/animations/rand.py
@@ -10,7 +10,7 @@ class rand(__animation.animation):
         self.initialize_argument("speed", "Speed: ", default_value=20)
         self.initialize_argument("count", "Count: ", default_value=1)
         self.initialize_argument("transition", "Transition time: ", default_value=30)
-        self.colours = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255), (255, 127, 0), (127, 255, 0), (127, 0, 255), (255, 0, 127), (0, 255, 127), (0, 127, 255)]
+        self.colours = [(255, 0, 0), (255, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255), (255, 127, 0), (127, 255, 0), (127, 0, 255), (0, 127, 255)]
     def run(self):
         while self.enabled:
             for x in range(int(self.get_argument("count"))):
